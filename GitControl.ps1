7e52b4d5b02cdb55586f4566ef13e84d2612d6e9
Aart den Hoed <adh.code@gmail.com>
2020-07-03 12:57:43 +0200

    Initial

---
+++ GitControl.ps1
@@ -0,0 +1,30 @@
+﻿CLS
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
+$Version = " -- Version: 1.0"
+$Node = " -- Node: " + $env:COMPUTERNAME
+$d = Get-Date
+$Datum = " -- Date: " + $d.ToShortDateString()
+$Tijd = " -- Time: " + $d.ToShortTimeString()
+$Scriptmsg = "PowerShell script " + $MyInvocation.MyCommand.Name + $Version + $Datum + $Tijd +$Node
+Write-Information $Scriptmsg 
+
+$FullScriptName = $MyInvocation.MyCommand.Definition
+$ScriptName = $MyInvocation.MyCommand.Name
+$ADHC_PsPath = $FullScriptName.Replace($ScriptName, "")
+$ADHC_InitVar = $ADHC_PsPath + "InitVar.PS1"
+& "$ADHC_InitVar"
+
+
+Set-Location -Path $devdir
+$gitdirs = Get-ChildItem "*.git" -Recurse
+foreach ($gitentry in $gitdirs) {
+    $dir = $gitentry.FullName
+   
+    $dir = $dir.replace(".git","")
+    Write-Information "Directory $dir"
+
+    Set-Location $dir
+    &git branch -v
+}
