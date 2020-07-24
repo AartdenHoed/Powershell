0fb1b7f29d82098ae9c56e55b460bd4132154bc0
Aart den Hoed <adh.code@gmail.com>
2020-07-24 09:48:43 +0200

    Examples apart

---
+++ MyExamples/ArpA.ps1
@@ -0,0 +1,47 @@
+﻿#Captures ARP Table then Displays ALL IPs that match the local computers IP Address
+CLS
+#Set the file you wish for the data to Output to
+$myfile = "D:\AartenHetty\OneDrive\ArpA\Test.txt"
+
+#Captures the devices IP Scheme and break it down, remove the Last Octet from the string
+#This section is only used IF you are trying to filter the ARP by the local network the device is on.
+$ComputerName = $env:computername
+$OrgSettings = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $ComputerName -EA Stop | ? { $_.IPEnabled }
+$myip = $OrgSettings.IPAddress[0]
+
+$ip = (([ipaddress] $myip).GetAddressBytes()[0..2] -join ".") + "."
+$ip = $ip.TrimEnd(".")
+Write-Warning "Executing on IP $myip, looking for ip-addresses in range $ip"
+
+#Searches the ARP table for IPs that match the scheme and parses out the data into an Array (It removed the Devices IP from the list.)
+
+$macarray = @()
+$arpa = (arp -a) 
+foreach ($line in $arpa) {
+    # Write-Warning "Line: $line"
+    $words =  $line.TrimStart() -split '\s+'
+    $thisIP = $words[0].Trim()
+    if ($thisIP -match $ip) {
+        $thisMac = $words[1] 
+        
+        Write-Warning "Handling IP address $thisIP with MacAddress $thisMac"
+        $obj = [PSCustomObject] [ordered]  @{IP = $thisIP; MAC = $thisMac}
+        
+        if (!($obj.MAC -eq "---" -or $obj.MAC -eq "Address" -or $obj.MAC -eq $null -or $obj.MAC -eq "ff-ff-ff-ff-ff-ff")) {
+            $macarray += $obj
+        }
+       
+    }
+   
+}
+            
+#Outputting the IP Addresses captured.
+remove-item $myfile -Force -ErrorAction SilentlyContinue 
+foreach ($entry in $macarray) {
+    
+    Export-Csv -InputObject $entry -Delimiter '~' -Force -Append `
+            -LiteralPath $myfile -NoTypeInformation
+}
+
+$macarray | Out-GridView    
+
---
+++ MyExamples/MacIP.ps1
@@ -0,0 +1,49 @@
+﻿CLS
+$lines = Get-Content -Path "D:\AartenHetty\OneDrive\ADHC Dev\Powershell/MacIP.txt"
+$i = 0
+$device = ""
+$IPaddress = ""
+$MACaddress = ""
+$overview = @()
+foreach ($line in $lines) {
+    
+    if ($line.trim() -eq "") {continue}
+    $i = $i + 1
+    switch ($i) {
+        1 {$device = $line.trim() }
+        2 {$IPaddress = $line.trim() }
+        3 {
+            $MACaddress = $line.trim()
+            $obj = [PSCustomObject] [ordered]  @{Device = $device; IPaddress = $IPaddress; MACaddress = $MACaddress;}
+        
+            $overview += $obj
+            $i = 0
+            $device = ""
+            $IPaddress = ""
+            $MACaddress = ""
+
+         }
+        default { Write-error "i = $i"}
+    }
+}
+$query = "TRUNCATE TABLE dbo.IPadressen" 
+invoke-sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "PRTG" `
+                -Query "$query" `
+                -ErrorAction Inquire
+Write-Warning "Dbo.IPadressen truncated"
+
+$n = 0
+foreach ($entry in $overview) {
+    $n = $n + 1
+    $query = "INSERT INTO [dbo].[IPadressen] ([Naam],[IPaddress],[MACaddress]) VALUES('" + 
+            $entry.Device + "','"+
+            $entry.IPaddress + "','"+
+            $entry.MACaddress + "')"
+    invoke-sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "PRTG" `
+                -Query "$query" `
+                -ErrorAction Inquire
+} 
+
+Write-Warning "$n entries inserted into dbo.IPadressen"
+
+$overview | Out-GridView
