5574eba7f5d1bc25b3ced0daded17544d0ee6ef1
Aart den Hoed <adh.code@gmail.com>
2020-08-04 18:30:17 +0200

    Geheel vernieuwd

--- ComparePROD.ps1
+++ ComparePROD.ps1
@@ -2,7 +2,7 @@
 $InformationPreference = "Continue"
 $WarningPreference = "Continue"
 
-$Version = " -- Version: 2.1"
+$Version = " -- Version: 3.1"
 $Node = " -- Node: " + $env:COMPUTERNAME
 $d = Get-Date
 $Datum = " -- Date: " + $d.ToShortDateString()
@@ -16,13 +16,130 @@ $ADHC_PsPath = $FullScriptName.Replace($ScriptName, "")
 $ADHC_InitVar = $ADHC_PsPath + "InitVar.PS1"
 & "$ADHC_InitVar"
 
-$StageLIst = Get-ChildItem $ADHC_StagingDir -Directory | Select Name,FullName
-
+# Init reporting file
 New-Item -ItemType Directory -Force -Path $ADHC_ProdCompareDir | Out-Null
 $Report = $ADHC_ProdCompareDir + "Report_" +  $ADHC_Computer  +  ".txt"
 Set-Content $Report $Scriptmsg -force
 
+# Proces development directories
+$DevList = Get-ChildItem $ADHC_DevelopDir -Directory | Select Name,FullName
+foreach ($DevDir in $DevLIst) {
+    $configfile = $DevDir.FullName + "\" + $ADHC_ConfigFile
+    [xml]$ConfigXML = Get-Content $configfile
 
+    # Get Staging info
+    $t = $ConfigXML.ADHCinfo.Stagelib.Directory
+    if ($t.'#text') {
+        $stagedir = $t.'#text'
+    }
+    else {
+        $stagedir = $t
+    }
+	$process = "COPY"
+	$c = $ConfigXML.ADHCinfo.Stagelib.Directory.Process
+	if ($c) {
+		$process = $c
+	}
+    
+	switch ($process.ToUpper()) {
+
+		"COPY" {
+			$DevContent = Get-ChildItem $Devdir.FullName -recurse -file  | `
+                                Where-Object {($_.FullName -notlike "*.git*") -and ($_.FullName -notlike "*MyExample*") } | `
+                                Select FullName,LastWriteTime,Length 
+			foreach ($devfile in $devcontent) {
+				$linewritten = $false
+		
+				# Check correctness of Staging directory
+				$stagename = $devfile.FullName.Replace($Devdir.FullName,$Stagedir)
+
+				if (Test-Path $stagename) {
+					$stageprops = Get-ItemProperty $stagename
+					$stagefound = $true
+				}
+				else {
+					$stagefound = $false
+				}
+
+				if (!$stagefound) {
+					Add-Content $Report " "
+					$msg = "Development file " + $devfile.FullName + " (Process = $process)"
+					Add-Content $Report $msg
+					$linewritten = $true
+					$msg = ">>> Staged file not found: " + $stagename
+					Add-Content $Report $msg
+            
+				} 
+				else {
+					$devprops = Get-ItemProperty $devfile.FullName
+					if (($devprops.Length -ne $stageprops.Length) -or ($devprops.LastWriteTime.ToString().Trim() -ne $stageprops.LastWriteTime.ToString().Trim())) {
+						Add-Content $Report " "
+						$msg = "Development file " + $devfile.FullName 
+						Add-Content $Report $msg
+						$linewritten = $true
+						$msg = "Length = " + $devprops.Length + " --- Last updated = " + $devprops.LastWriteTime
+						Add-Content $Report $msg
+						$msg = ">>> Staged file " + $stagename 
+						Add-Content $Report $msg
+						$msg = ">>> Length = " + $stageprops.Length + "--- Last updated = " + $stageprops.LastWriteTime
+						Add-Content $Report $msg
+                  
+					}
+
+				} 
+				
+
+				if (!(Test-Path $stagedir)) {
+					Add-Content $Report " "
+					$msg = ">>> Staging directory $stagedir niet gevonden"
+					Add-Content $Report $msg         
+				}
+				else {
+
+					$stagecontent = Get-ChildItem $stagedir -recurse -file  | Select FullName,LastWriteTime,Length 
+					foreach ($stagefile in $stagecontent) {
+					    $linewritten = $false
+
+					    # Check correctness of DEVELOPMENT directory
+					    $devname = $dslfile.FullName.Replace($stagedir,$DevDir.FullName)
+
+					    if (!(Test-Path $devname)) {
+						    if (!$linewritten) {
+							    Add-Content $Report " "
+							    $msg = "Staging file " + $stagefile.FullName
+							    Add-Content $Report $msg
+							    $linewritten = $true
+						    }
+						    $msg = ">>> Corresponding development file $devname not found for staged file " + $stagefile.FullName 
+						    Add-Content $Report $msg
+                              
+						    # Write-Warning "$devname niet gevonden"
+					    }
+					    else {
+						    # Write-Host "$devname wel gevonden"
+					    }
+
+					
+				    }
+
+        
+			    }
+
+    
+            }      
+		} 
+		
+		default {
+			Add-Content $Report " "
+            $msg = ">>> Staging directory $stagedir : process $process not implemented yet"
+            Add-Content $Report $msg
+            continue
+		}
+	}
+}
+
+# Proces staging directories
+$StageLIst = Get-ChildItem $ADHC_StagingDir -Directory | Select Name,FullName
 foreach ($StageDir in $StageLIst) {
     $configfile = $Stagedir.FullName + "\" + $ADHC_ConfigFile
     [xml]$ConfigXML = Get-Content $configfile
@@ -204,4 +321,8 @@ foreach ($StageDir in $StageLIst) {
         
 } 
 
+
+$msg = ">>> Script ended" 
+Add-Content $Report " "
+Add-Content $Report $msg
 exit
