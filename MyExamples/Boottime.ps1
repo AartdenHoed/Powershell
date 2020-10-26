74296ed2bc4535a70bcbf85e460abedc89a79f2c
Aart den Hoed <adh.code@gmail.com>
2020-10-26 14:46:09 +0100

    Voorbeelden

---
+++ MyExamples/Boottime.ps1
@@ -0,0 +1,18 @@
+﻿$i = Invoke-Expression('systeminfo | find /i "Boot Time"')
+$i
+$j = $i.Replace(" ", "").SPlit(" :,-")
+$day = $j[1]
+$month = $j[2]
+$year = $j[3]
+$hour = $j[4]
+$minute = $j[5]
+$second = $j[6]
+
+$day 
+$month 
+$year 
+$hour 
+$minute 
+$second 
+$boottime = Get-Date -Year $year -Month $month -Day $day -Hour $hour -Minute $minute -Second $second
+$boottime.ToString()
---
+++ MyExamples/UserError.ps1
@@ -0,0 +1,27 @@
+﻿class EnqErrorException : System.Exception  { 
+    $Node
+    $EnqName
+
+    EnqErrorException( [string]$message) : base($message) {
+
+    }
+
+    EnqErrorException() {
+
+    }
+}
+try {
+    CLS
+    $MyError = [EnqErrorException]::new("Enqueue for resource xxx failed on node yyy")
+    $MyError.Node = "ADHC"
+    $MyError.EnqName = "WMI"
+
+    throw $MyError
+}
+catch {
+    $x = $error[0].Exception | Select-Object -Property * 
+    $x
+    $x.Message
+    $x.node
+    $x.enqname
+}
