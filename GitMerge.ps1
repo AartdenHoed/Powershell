a211c242fc316613bcf28a22dd9fbfbc4c377e5c
Aart den Hoed <adh.code@gmail.com>
2020-12-22 22:13:43 +0100

    New

---
+++ GitMerge.ps1
@@ -0,0 +1,5 @@
+﻿cd "D:\AartenHetty\OneDrive\ADHC Development\Powershell.git"                                    # TO
+& git remote add mytemp 'D:\AartenHetty\OneDrive\ADHC Development\WindowsPowershell.git'        # FROM
+& git fetch mytemp 
+& git merge --allow-unrelated-histories mytemp/master 
+& git remote remove mytemp
