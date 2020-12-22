9b00481e4ad21cd0280433f41280174ae53d870c
Aart den Hoed <adh.code@gmail.com>
2020-12-22 22:14:15 +0100

    New

---
+++ MyExamples/Register.ps1
@@ -0,0 +1,10 @@
+﻿# Dit script zowel in 32 als in 64 bit mode draaien, in ADMIN mode!
+
+
+New-Item –Path "HKLM:\SOFTWARE\" –Name ADHC
+New-ItemProperty -Path "HKLM:\SOFTWARE\ADHC" -Name "SecurityString" -Value 'nZr4u7w!z%C*F-JaNdRgUkXp2s5v8y/A'  -PropertyType "String"
+
+
+
+$a = Get-ItemProperty -path HKLM:\SOFTWARE\ADHC | Select-Object -ExpandProperty "SecurityString"
+Write-Host "String = $a"
