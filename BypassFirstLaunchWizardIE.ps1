cc1fe8fe83d898270452091b75aa00ad1fa65161
Aart den Hoed <adh.code@gmail.com>
2021-05-25 21:35:46 +0200

    Example code to turn of "first launch wizard" bit in registry

---
+++ BypassFirstLaunchWizardIE.ps1
@@ -0,0 +1,3 @@
+﻿$keyPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Internet Explorer\Main'
+if (!(Test-Path $keyPath)) { New-Item $keyPath -Force | Out-Null }
+Set-ItemProperty -Path $keyPath -Name "DisableFirstRunCustomize" -Value 1
