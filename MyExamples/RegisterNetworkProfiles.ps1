0f54434333ecad407e1be2ce9024a764667c8a5f
Aart den Hoed <adh.code@gmail.com>
2021-06-16 21:42:43 +0200

    Cleanup of registry of obsolete network profiles

---
+++ MyExamples/RegisterNetworkProfiles.ps1
@@ -0,0 +1,36 @@
+﻿$a = Get-ChildItem -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles\"
+$n = 0
+foreach ($entry in $a) {
+    $n +=1
+    $p = $entry.GetValue("ProfileName")
+    $d = $entry.GetValue("Description")
+    
+    Write-host "==> Nummer $n"
+    Write-host $p, $d
+    $x = "Registry::" + $entry.name
+    $x
+
+    if ($d -eq "ExpressVPN") {
+        Remove-Item -literalpath "$x"    
+    
+    }
+    
+}
+$b = Get-ChildItem -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged"
+$n = 0
+foreach ($entry in $b) {
+    $n +=1
+    $p = $entry.GetValue("FirstNetwork")
+    $d = $entry.GetValue("Description")
+    
+    Write-host "==> Nummer $n"
+    Write-host $p, $d
+    $x = "Registry::" + $entry.name
+    $x
+
+    if ($d -like "*ExpressVPN*") {
+        Remove-Item -literalpath "$x"    
+        Write-Host "YES"
+    }
+    
+}
