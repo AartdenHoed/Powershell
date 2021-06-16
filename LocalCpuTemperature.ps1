169082330530aa28255d79a71e34c2b758c8ecf3
Aart den Hoed <adh.code@gmail.com>
2021-06-16 21:42:00 +0200

    New (temporary fix to bypass teh fact that PRTG sensor does not run in ADMIN mode)

---
+++ LocalCpuTemperature.ps1
@@ -0,0 +1,28 @@
+﻿CLS
+$errorcount = 0
+$loop = 0
+$myname = $MyInvocation.MyCommand.Name
+    
+$FullScriptName = $MyInvocation.MyCommand.Definition
+$mypath = $FullScriptName.Replace($MyName, "")
+
+$sensorscript = $mypath + "CpuTemperature.ps1"
+    & "$LocalInitVar"
+
+do {
+    $loop = $loop + 1
+    Start-Sleep -s 10
+    write-host "Loop $loop"
+    try {
+
+        & "$Sensorscript"
+
+    }
+    catch {
+
+        $errorcount += 1
+    
+    } 
+
+
+} Until ($errorcount -gt 10)
