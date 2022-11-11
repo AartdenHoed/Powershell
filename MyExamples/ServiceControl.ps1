29c0244cadb96669008ef820b3704ef2bd9aed14
ADHCode <adhc@hoedstorm.nl>
2022-11-11 23:09:46 +0100

    Move examples to right directory

---
+++ MyExamples/ServiceControl.ps1
@@ -0,0 +1,39 @@
+﻿cls
+$a = Get-WmiObject win32_service | select PSComputerName, SystemName, Name, Caption, Displayname,
+                                     PathName, ServiceType, StartMode, 
+                                     Started, State, Status, ExitCode, Description
+$AantalServices = $a.Count
+
+$b = $a | Sort-Object PathName 
+
+$AantalPaths = $b.count
+
+Write-Host "AantalServices Services met $AantalPaths Paths"
+
+$mylist = @()
+
+foreach ($service in $b) {
+    $thispath = $service.PathName
+    $ProgramName = ''
+    if ($thispath -match '"(.*?)"') {
+        $ProgramName = $matches[1]
+    }
+    else {
+        if ($thispath -match '(.*?)\s'){
+        $ProgramName = $matches[1]
+        }
+        else {
+            $ProgramName = $thispath    
+        }
+    }
+    if (-not $ProgramName) {
+        $ProgramName = "Unknown"
+        $service
+    }
+    $service | Add-Member NoteProperty ProgramName($ProgramName)
+    $mylist += $service
+}
+
+Write-Host "================="
+
+$mylist
--- ServiceControl.ps1
+++
@@ -1,5 +0,0 @@
-﻿$a = Get-WmiObject win32_service | select PSComputerName, SystemName, Name, Caption, Displayname,
-                                     PathNAme, ServiceType, StartMode, 
-                                     Started, State, Status, ExitCode, Description
-$a.count
-$a[8]
