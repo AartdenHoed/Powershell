72b7a186f4eff210bcf77956a1e14af806efe2d9
ADHCode <adhc@hoedstorm.nl>
2024-01-25 22:09:03 +0100

    Example to get certificates

---
+++ MyExamples/Certificate2.ps1
@@ -0,0 +1,20 @@
+﻿$a = Get-ChildItem -Path cert: -Recurse ! SELECT Subject, Thumbprint, FriendlyName
+$a
+foreach ($cert in $a) {
+    if ($cert.Thumbprint -ne $null) {
+        
+        $x = $cert.Thumbprint.ToString()
+        #if ($x -match "^E\d+") {
+        #    Write-Host "B I N G O"
+        #    Write-Host $x
+        #}
+        $y = $cert.FriendlyName
+        if ($y -match ".*hot.*") {
+            Write-Host "B I N G O"
+            Write-Host $y
+            Write-Host $cert
+        }
+        
+    }
+    # if ($cert.Thumbprint -eq )E5D49FE02E1F2BC92AF524AC75EAC3F89734D1C3
+}
