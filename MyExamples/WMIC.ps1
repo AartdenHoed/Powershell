70074f8a5d0d896853a4069347ac78ab20440e2e
ADHCode <adhc@hoedstorm.nl>
2024-03-21 08:53:45 +0100

    Poortnummer toegevogd voor benaderen PRTG via WEB

---
+++ MyExamples/WMIC.ps1
@@ -0,0 +1,7 @@
+﻿## WMIC /output: " + envir.wmictempname + " product get Name,Vendor,Version,InstallLocation,InstallDate"
+
+$a = WMIC  product get Name,Vendor,Version,InstallLocation,InstallDate
+
+$a[0]    ## is eerste regel , met de titels
+$a[1]    ## lege regel
+$a[2]    ## regel
